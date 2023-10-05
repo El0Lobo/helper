9e84c928707749a346c897dfcc76f104f8e2d5f3
El0Lobo <95318796+El0Lobo@users.noreply.github.com>
2023-10-05 14:43:35 +0200

    Add files via upload

---
+++ Xmarksthespot/dem_pirates.js
@@ -0,0 +1,38 @@
+const entries = {
+    "Stream": {
+        "English": {
+            "Fmovies": {
+                "link": "https://fmoviesz.to/home",
+                "nfo": "Excellent Site with Intro Skip and Auto Play"
+            },
+            "MyWatchseries": {
+                "link": "https://www.mywatchseries.cyou/",
+                "nfo": "Solid Streaming Side (many Videos are easy to Download)"
+            }
+        },
+        "Deutsch": {
+            "Kinox": {
+                "link": "https://kinox.mom/",
+                "nfo": "Modernes Kinox"
+            }
+        }
+    },
+    "Download": {
+        "Games": {
+            "IGG": {
+                "link": "https://igg-games.com/",
+                "nfo": "Somehow not trusted by Reddit, never had a problem..."
+            },
+            "Games3rb": {
+                "link": "https://www.game3rb.com/",
+                "nfo": "Best source for cracked Multiplayer "
+            }
+        },
+        "Software": {
+            "4download": {
+                "link": "https://4download.net/",
+                "nfo": "Solid Software Site"
+            }
+        }
+    }
+};
